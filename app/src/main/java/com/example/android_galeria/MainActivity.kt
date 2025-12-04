d3d182dc55b91e9f4c34c9da58a1b29a10f8297a
Alvarjur <aalvarojur@gmail.com>
2025-12-04 09:31:22 +0100

    cleaner

--- app/src/main/java/com/example/android_galeria/MainActivity.kt
+++ app/src/main/java/com/example/android_galeria/MainActivity.kt
@@ -12,13 +12,9 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var imageView: ImageView
-    // Registrar el ActivityResult una sola vez
     private val getContent = registerForActivityResult(GetContent()) { uri: Uri? ->
         imageView = findViewById(R.id.imageView)
-        // Aquí manejas la imagen seleccionada
         if (uri != null) {
-            // Ejemplo: imprimir la URI
-            println("Imagen seleccionada: $uri")
             imageView.setImageURI(uri)
         }
     }
@@ -28,17 +24,8 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // Ajuste de insets
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        // Referencia al botón
         val selectButton = findViewById<Button>(R.id.button)
 
-        // Listener para abrir el selector de imágenes
         selectButton.setOnClickListener {
             getContent.launch("image/*")
         }
