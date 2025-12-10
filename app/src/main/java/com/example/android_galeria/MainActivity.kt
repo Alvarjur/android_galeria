938503b0608ae5d97d3f4c7f60c9223858a8b020
Alvarjur <aalvarojur@gmail.com>
2025-12-10 18:52:42 +0100

    thumb done ig

--- app/src/main/java/com/example/android_galeria/MainActivity.kt
+++ app/src/main/java/com/example/android_galeria/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.GetContent
 import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -18,6 +20,11 @@ class MainActivity : AppCompatActivity() {
             imageView.setImageURI(uri)
         }
     }
+    private val takeThumb = registerForActivityResult(
+        ActivityResultContracts.TakePicturePreview()) { bitmap: Bitmap? ->
+        imageView = findViewById(R.id.imageView)
+        imageView.setImageBitmap(bitmap)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +32,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val selectButton = findViewById<Button>(R.id.button)
+        val thumbButton = findViewById<Button>(R.id.thumbButton)
 
         selectButton.setOnClickListener {
             getContent.launch("image/*")
+        }
+        thumbButton.setOnClickListener {
+            takeThumb.launch(null)
         }
     }
 }
